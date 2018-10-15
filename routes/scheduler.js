134d1d150ec9d8b3bec3fae649f75e9aa7848ac8
YevhenSlyva <yevhen.slyva@nure.ua>
2018-10-15 21:44:20 +0300

    Remove charackter encoding cp1251

--- routes/scheduler.js
+++ routes/scheduler.js
@@ -4,8 +4,7 @@ var nureApi = require('./../modules/nure_api');
 
 router.get('/', async function(req, res, next) {
   var fromDate = new Date(req.query.fromDate);
-  var toDate = new Date(req.query.toDate);  
-  res.set({ 'content-type': 'application/json; charset=windows-1251' });
+  var toDate = new Date(req.query.toDate);    
   res.send(JSON.stringify(await nureApi.getSchedule(5259418, fromDate, toDate)));  
 });
 
